0d83945799e22bfd9e088bbb9f17dac6a1c0393e
Engin Ozcelik <enginozcelik93@gmail.com>
2020-02-14 02:17:20 +0300

    update 02/14/20

--- ac-gen-keras-ASLR.py
+++ ac-gen-keras-ASLR.py
@@ -109,7 +109,7 @@ def build_discriminator():
 
 if __name__ == '__main__':
     # batch and latent size taken from the paper
-    epochs = 1000
+    epochs = 6000
     batch_size = 100
     latent_size = 100
 
