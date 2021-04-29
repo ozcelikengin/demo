35f4bbce7ec12dc2f57bc2cc328f2a6cde18b4ce
Engin Ozcelik <enginozcelik93@gmail.com>
2021-04-30 02:29:22 +0300

    up

---
+++ keras.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+model = keras.Sequential(
+    [
+        layers.Dense(2, activation="relu", name="layer1"),
+        layers.Dense(3, activation="relu", name="layer2"),
+        layers.Dense(4, name = "layer3"),
+    ]
+)
+
+x = tf.ones((3,3))
+y = model(x)
